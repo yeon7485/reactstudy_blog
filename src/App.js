649c4468ef998d7f7c5dfefdbf5aac19b45d00b7
yeon7485 <wsx4379@naver.com>
2022-02-09 23:08:41 +0900

    state로 변경하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [title, updateTitle] = useState(["여자 코트 추천", "강남 맛집 추천", "내돈내산 리뷰"]);
+  let [like, updateLike] = useState(0);
+
+  function updateFirst() {
+    let newArray = [...title];
+    newArray[0] = "예쁜 코트 추천"
+    updateTitle(newArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안뇽하세요!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3> {title[0]} <span onClick={() => {
+          updateLike
+            (like + 1)
+        }}>💜</span> {like} </h3>
+        <button onClick={updateFirst}>제목 변경</button>
+        <p>2월 9일</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {title[1]} </h3>
+        <p>2월 9일</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {title[2]} </h3>
+        <p>2월 8일</p>
+        <hr />
+      </div>
+
+      <div className='model'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+
     </div>
   );
 }
